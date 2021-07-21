010e01297e15b4abe08e30a78faeee9accf5c86f
Mo <mohammad.r.ashouri@gmail.com>
2021-07-21 19:27:11 -0400

    ...

---
+++ lib.rs
@@ -0,0 +1,21 @@
+#![deny(missing_docs)]
+#![forbid(unsafe_code)]
+
+//! A multisig wallet program for the Solana blockchain Designed by Mohammadreza Ashouri / ashourics@gmail.com
+
+#[macro_use]
+extern crate serde_derive;
+
+pub mod error;
+pub mod instruction;
+pub mod processor;
+pub mod state;
+pub mod utils;
+
+#[cfg(not(feature = "no-entrypoint"))]
+pub mod entrypoint;
+
+// Export current sdk types for downstream users building with a different sdk version
+pub use solana_program;
+
+solana_program::declare_id!("WaLLeTnuVFfoVWyTs5XjPwJSWd2Ttdd1PrZ7VE3zsUV");
